96475286131251b68bd554952cc1be81a2adbfd1
musakanneh <mkanneh17@alustudent.com>
2021-03-16 21:35:20 +0200

    First commit

--- app.py
+++
@@ -1,9 +0,0 @@
-class Solution():
-    """ Model product rating """
-
-    def __init__(self):
-        return
-
-    def user_input(self):
-        """ Accepts input from users """
-        pass                                                                                                                                                                                                                                                                                                                                    
