113821ede28bfce0406c3a62acb27d32b3bec725
musakanneh <mkanneh17@alustudent.com>
2021-03-18 10:28:18 +0200

    More CSS

--- build_model.py
+++ build_model.py
@@ -1,4 +1,5 @@
-import pandas as pd, numpy as np
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -8,29 +9,45 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.externals import joblib
 from scipy import sparse
-import gzip, re, string
+import gzip
+import re
+import string
 
 import warnings
 warnings.filterwarnings('ignore')
 
 if __name__ == "__main__":
-    
+
     def parse(path):
+        """Create path for parsing the model
+
+        Args:
+            path(args): path to locate the model
+
+        """
         g = gzip.open(path, 'rb')
         for l in g:
             yield eval(l)
 
     def getDF(path):
-        i = 0
+        """Returns a dictionary representation
+        of the dataframe
+
+        Args:
+            path(args): path to locate the data model
+        """
+        count_iteration = 0
         df = {}
-        for d in parse(path):
-            df[i] = d
-            i += 1
+        for data in parse(path):
+            df[i] = data
+            count_iteration += 1
         return pd.DataFrame.from_dict(df, orient='index')
 
     def pr(x, y_i, y):
-        p = x[y==y_i].sum(0)
-        return (p+1) / ((y==y_i).sum()+1)
+        """
+        """
+        p = x[y == y_i].sum(0)
+        return (p+1) / ((y == y_i).sum()+1)
 
     category_names = ['Automotive', 'Baby', 'Clothing_Shoes_and_Jewelry',
                       'Digital_Music', 'Electronics', 'Grocery_and_Gourmet_Food',
@@ -44,15 +61,16 @@ if __name__ == "__main__":
 
         treated_text = []
         for index, row in df.iterrows():
-            treated_text.append(re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])').sub(r' \1 ', row['reviewText']))
-        
-        vec = TfidfVectorizer(ngram_range=(1,2),
-                min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
-                smooth_idf=1, sublinear_tf=1 )
+            treated_text.append(re.compile(
+                f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])').sub(r' \1 ', row['reviewText']))
+
+        vec = TfidfVectorizer(ngram_range=(1, 2),
+                              min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
+                              smooth_idf=1, sublinear_tf=1)
         x = vec.fit_transform(treated_text)
         y = (df['overall'] > 3).astype(float).values
-        r = sparse.csr_matrix(np.log(pr(x,1,y) / pr(x,0,y)))
-        clf = LogisticRegression(C = 10, dual=True, penalty = "l2", n_jobs = -1)
+        r = sparse.csr_matrix(np.log(pr(x, 1, y) / pr(x, 0, y)))
+        clf = LogisticRegression(C=10, dual=True, penalty="l2", n_jobs=-1)
         x_nb = x.multiply(r)
         clf.fit(x_nb, y)
         # pickle your model for each category
